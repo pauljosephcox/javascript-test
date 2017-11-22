bc77f3cebbbd4d2f700aa754159fbd60e92c7e7d
Paul Cox <pauljosephcox@gmail.com>
2017-11-22 22:05:36 +1100

    Modules

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,17 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
+      var mod = {
 
+          greeting: str1,
+
+          name: str2,
+
+          sayIt: function(){
+              return this.greeting +', ' + this.name;
+          }
+
+      };
+      return mod;
   }
 };
