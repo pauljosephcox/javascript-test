8b81d678869a628c899cd45a07a33bf9f77ddd15
Paul Cox <pauljosephcox@gmail.com>
2017-11-22 20:36:47 +1100

    counter

--- app/count.js
+++ app/count.js
@@ -2,54 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-      //
-      // let interval = setInterval(function(){
-      //
-      //     // if(start >= end) clearInterval(interval);
-      //     // else {
-      //     //
-      //     //     start++;
-      //     //     console.log(start);
-      //     // }
-      //     console.log("HELLO WORLD");
-      //
-      // },100);
-      // return this;
-      // if(start >= end) clearInterval(interval);
-      // else {
-      //
-      //     console.log(start);
-      //     start++;
-      //     setInterval(function(){
-      //
-      //     })
-      //     this.count(start,end);
-      //
-      //
-      // }
-      // setTimeout(function(){ alert("Hello"); }, 3000);
-      // console.log(start);
 
-      // console.log(start);
+      console.log(start);
 
-      this.interval = setInterval(function () {
-
-          if(start === end) {
-              this.cancel();
-          } else {
-
-              start++;
-              // return start;
-
-          }
-
+      this.interval = setInterval(() => {
+        if (start === end) {
+          this.cancel();
+        } else {
+          start++;
+          console.log(start);
+        }
       }, 100);
-
-      this.cancel = function(){
-          clearInterval(this.interval);
-      }
 
       return this;
 
-  }
+    },
+    cancel: function(){
+        clearInterval(this.interval);
+    }
+
 };
