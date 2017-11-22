9e43b41db51a6c613d1c2e57a6bd3d47f9eed9bd
Paul Cox <pauljosephcox@gmail.com>
2017-11-22 22:20:21 +1100

    object

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,24 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
+
   alterContext: function(fn, obj) {
+
+      return fn.call(obj);
 
   },
 
   alterObjects: function(constructor, greeting) {
-
+      return constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
+      let props = [];
+      for(let i in obj){
 
+          if(obj.hasOwnProperty(i)) props.push(i+': '+obj[i]);
+          
+      }
+      return props;
   }
 };
