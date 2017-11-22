c736abd878637b743dc1718117b47552c690c033
Paul Cox <pauljosephcox@gmail.com>
2017-11-22 17:29:47 +1100

    array, async, bestpractice

--- app/arrays.js
+++ app/arrays.js
@@ -15,6 +15,9 @@ exports.arraysAnswers = {
 
   removeWithoutCopy: function(arr, item) {
 
+      for(var i in arr) if(arr[i] == item) arr.splice(i,1);
+      if(arr.indexOf(item) > -1) this.removeWithoutCopy(arr,item);
+      return arr;
   },
 
   append: function(arr, item) {
@@ -23,7 +26,8 @@ exports.arraysAnswers = {
   },
 
   truncate: function(arr) {
-
+      arr.pop();
+      return arr;
   },
 
   prepend: function(arr, item) {
@@ -32,30 +36,48 @@ exports.arraysAnswers = {
   },
 
   curtail: function(arr) {
-
+      arr.shift();
+      return arr;
   },
 
   concat: function(arr1, arr2) {
-
+      return arr1.concat(arr2);
   },
 
   insert: function(arr, item, index) {
-
+      arr.splice(index,0,item);
+      return arr;
   },
 
   count: function(arr, item) {
+
+      let total = 0;
+      for(let i in arr) if(arr[i] == item) total++;
+      return total;
 
   },
 
   duplicates: function(arr) {
 
+      var all = [];
+      var dups = [];
+      for(let i in arr){
+
+          if((all.indexOf(arr[i]) > -1) && dups.indexOf(arr[i]) <= -1) dups.push(arr[i]);
+          all.push(arr[i]);
+
+      }
+      return dups;
+
   },
 
   square: function(arr) {
-
+      return arr.map((i) => i*i);
   },
 
   findAllOccurrences: function(arr, target) {
-
+      let indexes = [];
+      for(let i in arr) if(arr[i] == target) indexes.push(i);
+      return indexes;
   }
 };
--- app/async.js
+++ app/async.js
@@ -2,10 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+      return new Promise((resolve, reject) => {resolve(value)});
   },
 
   manipulateRemoteData: function(url) {
 
+      return new Promise(function(resolve, reject){
+
+          $.ajax(url).done(function(data){
+
+              resolve(data.people.map(person => person.name).sort());
+
+          })
+          
+      });
   }
 };
